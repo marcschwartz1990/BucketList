966ab272c5466472f40cb864e9426702bd486f53
Marc Schwartz <marcschwartz1990@gmail.com>
2022-10-04 23:28:04 -0400

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Marc-Developer on 10/4/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
